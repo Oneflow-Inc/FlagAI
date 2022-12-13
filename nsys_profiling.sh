919250edbabfea962ce6ad4858c71a7d442af653
luyang <flowingsun007@163.com>
2022-12-13 03:13:49 +0000

    add nsys profiling script

---
+++ nsys_profiling.sh
@@ -0,0 +1,15 @@
+# set nsight system path
+nsys_path=/usr/local/cuda/nsight-systems-2022.1.3
+
+# set params
+ddim_steps=20
+sed -i "s/^ddim_steps = [[:digit:]]*/ddim_steps = $ddim_steps/g " diffusion_demo.py
+
+# save to nsys file
+time=$(date "+%Y%m%d%H")
+git_commit=$(python3 -m oneflow --doctor | grep "git_commit: ")
+$nsys_path/bin/nsys profile --stats=true -o altdiffusion_${time}_${ddim_steps}steps_flow@${git_commit#*: }.nsys-rep python3 diffusion_demo.py
+
+
+# checkout changes
+git checkout diffusion_demo.py
